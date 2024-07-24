99f218a2f029f1fcaa258fc27a9888fdc3d26780
Bahadyr <mahmutbahadrozturk@gmail.com>
2024-07-24 11:50:31 -0700

    change from java 11 to 18

--- src/main/java/com/selenium/Main.java
+++ src/main/java/com/selenium/Main.java
@@ -6,13 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
         //Creating webdriver instance
         WebDriver driver= new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
---
+++ src/main/java/com/selenium/Study.java
@@ -0,0 +1,8 @@
+package com.selenium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Study {
+private List<String> classes = new ArrayList<String>();
+}
